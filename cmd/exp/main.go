7dc8c46bf5025a6a5c6dbf4693bfa857bf91128a
ysshuklashreyank <shreyank.shukla.rishu@gmail.com>
2023-09-30 15:17:09 +0530

    Template exercises

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"html/template"
 	"os"
+	"time"
 )
 
 type User struct {
-	Name string
-	Age  int
-	Bio  string
-	Meta UserMeta
+	Name    string
+	Age     int
+	Bio     string
+	Meta    UserMeta
+	Contact int
+	DOB     time.Time
+	Langs   []string
 }
 
 type UserMeta struct {
@@ -27,7 +31,10 @@ func main() {
 		Meta: UserMeta{
 			Visits: 10,
 		},
-		Bio: `<script>alert("Yo");</script>`,
+		Bio:     `<script>alert("Yo");</script>`,
+		Contact: 128493492349,
+		DOB:     time.Date(2000, 2, 30, 0, 0, 0, 0, time.Local),
+		Langs:   []string{"Hindi", "English"},
 	}
 
 	err = t.Execute(os.Stdout, user)
