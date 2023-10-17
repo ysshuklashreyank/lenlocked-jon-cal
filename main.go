8c45bafade9638b476b6033019a2cc83cfcf2a19
Shreyank Shukla <shreyank.shukla.rishu@gmail.com>
2023-10-17 18:10:39 +0530

    Added the users.Create method to handle the signup form

--- main.go
+++ main.go
@@ -30,6 +30,8 @@ func main() {
 	usersC := controllers.Users{}
 	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
 	r.Get("/signup", usersC.New)
+	r.Post("/users", usersC.Create)
+
 	r.Get("/galleries/{id}", galleriesHandler)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
