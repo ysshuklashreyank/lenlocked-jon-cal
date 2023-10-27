b7244dde05118d20ea4202cd63a0feb53d632253
Shreyank Shukla <shreyank.shukla.rishu@gmail.com>
2023-10-27 22:42:18 +0530

    Process sign in

--- main.go
+++ main.go
@@ -50,6 +50,7 @@ func main() {
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
 	r.Get("/signin", usersC.SignIn)
+	r.Post("/signin", usersC.ProcessSignIn)
 
 	r.Get("/galleries/{id}", galleriesHandler)
 
