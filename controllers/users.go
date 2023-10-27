56c12725490c9e40ab73dd34801a8d9dd58adc12
Shreyank Shukla <shreyank.shukla.rishu@gmail.com>
2023-10-27 22:17:17 +0530

    'sign in view'

--- controllers/users.go
+++ controllers/users.go
@@ -9,7 +9,8 @@ import (
 
 type Users struct {
 	Templates struct {
-		New Template
+		New    Template
+		SignIn Template
 	}
 	UserService *models.UserService
 }
@@ -31,4 +32,12 @@ func (u Users) Create(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Something went wrong", http.StatusInternalServerError)
 	}
 	fmt.Fprintf(w, "User created: %v", user)
+}
+
+func (u Users) SignIn(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Email string
+	}
+	data.Email = r.FormValue("email")
+	u.Templates.SignIn.Execute(w, data)
 }
--- main.go
+++ main.go
@@ -41,9 +41,15 @@ func main() {
 	usersC := controllers.Users{
 		UserService: &userService,
 	}
-	usersC.Templates.New = views.Must(views.ParseFS(templates.FS, "signup.gohtml", "tailwind.gohtml"))
+	usersC.Templates.New = views.Must(views.ParseFS(
+		templates.FS, "signup.gohtml", "tailwind.gohtml",
+	))
+	usersC.Templates.SignIn = views.Must(views.ParseFS(
+		templates.FS, "signin.gohtml", "tailwind.gohtml",
+	))
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
+	r.Get("/signin", usersC.SignIn)
 
 	r.Get("/galleries/{id}", galleriesHandler)
 
